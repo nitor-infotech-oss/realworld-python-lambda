62c7ddb09055399d0c436cefea71e036181ba312
gautamp25 <45966893+gautamp25@users.noreply.github.com>
2019-08-14 20:22:42 +0530

    follow/unfollow user implementation

--- api/user.py
+++ api/user.py
@@ -3,14 +3,18 @@ import json
 import logging
 import uuid
 import time
-from datetime import datetime
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 # import bcrypt
 # import jwt
 from src.util import envelop
+from datetime import datetime, timedelta
 
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS256'
+JWT_EXP_DELTA_SECONDS = 172800  # 2 days
 
 
 # create user
@@ -48,7 +52,6 @@ def create_user(event, context):
     # password = data['password'].encode()
     # hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     # encoded = jwt.encode({'token': data['username']}, 'secret', algorithm='HS256')
-    # print(encoded)
     item = {
         # 'id': str(uuid.uuid1()),
         'username': data['username'],
@@ -63,6 +66,7 @@ def create_user(event, context):
     token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6ImdhdXRhbSJ9.5CLsX4nOuTagsC6nSGWfq-4oZZAL0RhlMLOm7QMGy_Q'
     body = {
         'email': data['email'],
+        # 'token': min_token(data['username']),
         'token': token,
         'username': data['username'],
         'bio': '',
@@ -81,6 +85,15 @@ def create_user(event, context):
     return response
 
 
+def min_token(a_username):
+    payload = {
+        'username': a_username,
+        'expiresIn': datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS)
+    }
+    jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
+    return jwt_token
+
+
 def get_user_by_username(username):
     table = dynamodb.Table('dev-users')
     print(username)
@@ -96,14 +109,13 @@ def get_user_by_username(username):
     return response
 
 
-def get_user_by_email(aemail):
+def get_user_by_email(a_email):
     table = dynamodb.Table('dev-users')
-    # print(aemail)
     response = table.query(
         IndexName='email',
         KeyConditionExpression='email= :email',
         ExpressionAttributeValues={
-            ':email': aemail,
+            ':email': a_email,
         },
         Select='ALL_ATTRIBUTES',
     )
@@ -137,6 +149,7 @@ def login_user(event, context):
 
     authenticated_user = {
         'email': data['email'],
+        # 'token': min_token(get_user_with_this_email['Items'][0]['username']),
         'token': token,
         'username': get_user_with_this_email['Items'][0]['username']
         # 'bio': get_user_with_this_email['Items'][0]['bio'] if get_user_with_this_email['Items'][0]['bio'] else '',
@@ -256,6 +269,84 @@ def get_profile(event, context):
     return response
 
 
+def follow(event, context):
+    authenticated_user = authenticate_and_get_user(event, context)
+    if not authenticated_user:
+        raise Exception('Token not present or invalid.', 422)
+    username = event['pathParameters']['username']
+    user = (get_user_by_username(username))['Item']
+    should_follow = (not event['httpMethod'] == 'DELETE')
+
+    # Update "followers" field on followed user
+    if should_follow:
+        if user['followers'] and authenticated_user['username'] not in user['followers']:
+            pass
+            user['followers'].append(authenticated_user['username'])
+        else:
+            user['followers'] = [authenticated_user['username']]
+    else:
+        if user['followers'] and authenticated_user['username'] in user['followers']:
+            # create new list of follower except authenticated user
+            follow_result = filter(lambda x: x != authenticated_user['username'], user['followers'])
+            user['followers'] = list(follow_result)
+
+            # delete followers if list is empty
+            if not len(user['followers']):
+                print("In Delete condition")
+                del user['followers']
+
+    table = dynamodb.Table('dev-users')
+    table.put_item(Item=user)
+
+    # Update "following" field on follower user
+    if should_follow:
+        if authenticated_user['following'] and authenticated_user['following'] not in username:
+            authenticated_user['following'].append(username)
+        else:
+            authenticated_user['following'] = [username]
+    else:
+        if authenticated_user['following'] and authenticated_user['following'] in username:
+            # create new list of following except username
+            result = filter(lambda x: x != username, authenticated_user['following'])
+            authenticated_user['following'] = list(result)
+
+            # delete following if list is empty
+            if not len(authenticated_user['following']):
+                del authenticated_user['following']
+
+    table.put_item(Item=authenticated_user)
+
+    profile = {
+        'username': username,
+        'bio': user['bio'] if user['bio'] else '',
+        'image': user['image'] if user['bio'] else '',
+        'following': should_follow
+    }
+
+    response = {
+        "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': 'true'
+        },
+        "profile": profile
+    }
+
+    return response
+
+
+# create followed users
+def get_followed_users(a_username):
+    table = dynamodb.Table('dev-users')
+    user = table.get_item(
+        Key={
+            'username': a_username
+        }
+    )['Item']
+
+    return user['following'] or user['following']== []
+
+
 def get_token_from_event(event, context):
     pass
     # return event.headers.Authorization.replace('Token ', '')
@@ -276,8 +367,8 @@ def get_profile_by_username(a_username, a_authenticated_user):
     }
 
     # If user is authenticated, set following bit
-    # if user['followers'] and a_authenticated_user:
-    #     profile['following'] = user['followers'].values.includes(a_authenticated_user.username)
+    if user['followers'] and a_authenticated_user:
+        profile['following'] = user['followers']in a_authenticated_user['username']
 
     return profile
 
